9a18dc9c3939d37970c2a0335209c70b7d26151b
Thiago Paz <thiago.paz.extern@userlane.com>
2024-07-31 16:10:29 +0100

    enable tests

--- app/calculator.py
+++ app/calculator.py
@@ -64,23 +64,23 @@ class Calculator:
     def tangent(x):
         return math.tan(x)
 
-# # Example usage:
-# calc = Calculator()
+# Example usage:
+calc = Calculator()
 
-# print(calc.add(10, 5))           # 15
-# print(calc.subtract(10, 5))      # 5
-# print(calc.multiply(10, 5))      # 50
-# print(calc.divide(10, 5))        # 2.0
-# print(calc.divide(10, 0))        # 'Cannot divide by 0'
-# print(calc.modulus(10, 3))       # 1
-# print(calc.modulus(10, 0))       # 'Cannot perform modulus operation with 0'
-# print(calc.power(2, 3))          # 8
-# print(calc.sqrt(16))             # 4.0
-# print(calc.sqrt(-16))            # 'Cannot take square root of a negative number'
-# print(calc.log(10))              # 2.302585092994046 (natural log)
-# print(calc.log(100, 10))         # 2.0 (log base 10)
-# print(calc.factorial(5))         # 120
-# print(calc.factorial(-5))        # 'Factorial is not defined for negative numbers'
-# print(calc.sine(math.pi / 2))    # 1.0
-# print(calc.cosine(math.pi))      # -1.0
-# print(calc.tangent(math.pi / 4)) # 1.0
+print(calc.add(10, 5))           # 15
+print(calc.subtract(10, 5))      # 5
+print(calc.multiply(10, 5))      # 50
+print(calc.divide(10, 5))        # 2.0
+print(calc.divide(10, 0))        # 'Cannot divide by 0'
+print(calc.modulus(10, 3))       # 1
+print(calc.modulus(10, 0))       # 'Cannot perform modulus operation with 0'
+print(calc.power(2, 3))          # 8
+print(calc.sqrt(16))             # 4.0
+print(calc.sqrt(-16))            # 'Cannot take square root of a negative number'
+print(calc.log(10))              # 2.302585092994046 (natural log)
+print(calc.log(100, 10))         # 2.0 (log base 10)
+print(calc.factorial(5))         # 120
+print(calc.factorial(-5))        # 'Factorial is not defined for negative numbers'
+print(calc.sine(math.pi / 2))    # 1.0
+print(calc.cosine(math.pi))      # -1.0
+print(calc.tangent(math.pi / 4)) # 1.0
